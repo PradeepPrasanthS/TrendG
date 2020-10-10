877d259f2c211884b68d8e34cefb182b3b049f01
Pradeep Prasanth S <pradeepprasanths@gmail.com>
2020-10-10 20:56:25 +0530

    - Code refactoring and code optimisation

--- app/src/main/java/com/pradeep/trendg/view/HomeActivity.kt
+++ app/src/main/java/com/pradeep/trendg/view/HomeActivity.kt
@@ -1,9 +1,7 @@
 package com.pradeep.trendg.view
 
-import android.content.Context
 import android.os.Bundle
 import android.view.Menu
-import android.view.View
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -17,23 +15,15 @@ class HomeActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private lateinit var viewModel: HomeViewModel
     private lateinit var adapter: TrendingGitListAdapter
-    private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        context = this
-
         supportActionBar?.let {
             it.title = getString(R.string.title_trending_repositories)
             it.elevation = 0f
         }
-
-        val layoutManager = LinearLayoutManager(this)
-        trendingGitList.layoutManager = layoutManager
-
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         init()
 
@@ -48,8 +38,10 @@ class HomeActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     }
 
     private fun init() {
-        trendingGitList.visibility = View.VISIBLE
-        noInternetLayout.visibility = View.GONE
+        val layoutManager = LinearLayoutManager(this)
+        trendingGitList.layoutManager = layoutManager
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
         viewModel.repositoryData.observe(this, { trendingGit ->
             adapter = TrendingGitListAdapter(trendingGit, application)
             trendingGitList.adapter = adapter
